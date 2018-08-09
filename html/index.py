0aa91fd67229cef65cd36eee273f7b4d1ccb48f1
Fisher Chase <Haeshka@gmail.com>
2018-08-09 12:26:02 -0600

    Created the basic template layouts
    layout and display_unit.html files.
    used "display_unit" as the block names.

--- html/index.py
+++ html/index.py
@@ -1,4 +1,8 @@
-import flask
+from flask import Flask, render_template
+
+# This is not an actual, operational file.
+# This file is solely here as a blueprint to remind that all variables and data
+## need to be pushed through flask and its render.
 
 print(""" <html>
 <head>
@@ -10,3 +14,19 @@ print(""" <html>
 	{{ variable }}
 </body>
 </html>""")
+
+
+## likely variables and data.
+
+unitdata = [] # replace with a method from the class.unit.py file.
+## Expressly asks for all of the unit's data, to be dumped into a single array.
+
+page_name = ''
+page_title = '' # Use a Standard:
+## Home
+## Unit: Unit Name
+## Army List: Army Name
+## Edit Army: Army Name
+### Format should be 'Home' + page_name
+
+logged_in = # booleanself.
