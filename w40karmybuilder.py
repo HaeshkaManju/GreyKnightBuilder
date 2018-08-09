8fb26c135b55d708a6a2bd43d9a8fa7b522147e7
Fisher Chase <Haeshka@gmail.com>
2018-08-09 16:15:00 -0600

    Separate "make it run" branch
    This branch will run, but, takes-away from the routing.

--- w40karmybuilder.py
+++ w40karmybuilder.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# This file is part of the render(ing) domainself.
+# This file is part of the render(ing) domain.
 # Once this file is established, tested and proven to function, as intended - do not modify the existing code, only extend from it.
 
 ## use decorates for handling the URL structure.  Do not hard-code URL structure with HTML files.
@@ -14,29 +14,17 @@ app = Flask(__name__)
 ### Page Title, Header Tags (if applicable), Block Content, Block Content Variables, form content, form content submission options.
 
 # Self Note: two decorators directly following allows setting the same define function to the same route.
-if logged_in = True:
-    @app.route("/")
-    @app.route("/home")
-    def home():
-        return render_template('dashboard.html', title=home_title, dashboard_block=True, sign_up_block=False, login_block=False, logout_block=True)
-else:
-    @app.route("/")
-    @app.route("/home")
-    def home():
-        return render_template('home.html', title=home_title, dashboard_block=False, sign_up_block=True, login_block=True, logout_block=False)
+# @app.route("/")
+# @app.route("/home")
+# def home():
+#    return render_template('home.html')
 
-if logged_in = True:
-    @app.route("/view_unit.html")
-    def view_unit():
-        return render_template('view_unit.html', title=view_unit, logout_block=False)
-else:
-    @app.route("/home")
-    def home():
-        return render_template('home.html', title=home_title, dashboard_block=False, sign_up_block=True, login_block=True)
-
+@app.route("/display_unit.html")
+def display_unit():
+    return render_template('display_unit.html')
 
 ### For the love of all that is holy - do not run this block in a production environment. ###
-if__name__== '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
 # comment-out the above block when in production.
 # Above block to be used in test environment.
